7cc62c1be19ce45930f7d1ca16ed28be45b7e591
Eric Christensen <hebrew.8.12@gmail.com>
2023-05-13 13:40:57 -0700

    0.1.0

--- src/routes/(app)/benchmarks/+page.svelte
+++ src/routes/(app)/benchmarks/+page.svelte
@@ -1,1 +1,17 @@
+<script>
+	async function builder() {
+		const response = await fetch('/api/admin/system', {
+			method: 'GET',
+			// @ts-ignore
+			headers: {
+				'content-type': 'application/json'
+			}
+		});
+
+		console.log(await response.json());
+	}
+</script>
+
 <h3>HTTP Benchmarks</h3>
+
+<button on:click={builder}>Build</button>
